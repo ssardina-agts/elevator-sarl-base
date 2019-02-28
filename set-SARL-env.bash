7f332f214bf91247eb3e134e3d815c2d8f13ca98
Sebastian Sardina <ssardina@gmail.com>
2019-02-28 19:46:19 +1100

    Upgraded Maven to latest SARL-PROLOG-CAP

--- set-SARL-env.bash
+++ set-SARL-env.bash
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-export LD_PRELOAD=$LD_PRELOAD:libswipl.so
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/swi-prolog/lib/amd64/
+# For Linux-based official distribution install
+export SWI_HOME_DIR=/usr/lib/swi-prolog
+export LD_LIBRARY_PATH=$SWI_HOME_DIR/lib/x86_64-linux/:$SWI_HOME_DIR/lib/amd64/:$LD_LIBRARY_PATH
+
+# For a local install under /usr/local/swipl-git
+# Uncomment and adapt the path if you have compiled and installed SWI manually from repo
+#export SWI_HOME_DIR=/usr/local/swipl-git/lib/swipl/
+#export LD_LIBRARY_PATH=$SWI_HOME_DIR/lib/x86_64-linux/:$SWI_HOME_DIR/lib/amd64/:$LD_LIBRARY_PATH
+
+
+# This is actually only needed if your execution complains because it has to be pre-loaded
+# Check https://answers.ros.org/question/132411/unable-to-load-existing-owl-in-semantic-map-editor/
+export LD_PRELOAD=libswipl.so:$LD_PRELOAD
+
+# Set SARL VERSION
 export SARL_VERSION=0.8.6
-
-
